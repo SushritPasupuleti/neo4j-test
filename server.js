8d5db1a30639300b227f707556e33f687936a187
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-09-04 17:54:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const neo4j = require('neo4j-driver')
+
 const port = 5000
+const uri = 'bolt://localhost'
+//Middleware//
+app.use(bodyParser.json());
+//Middleware//
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
