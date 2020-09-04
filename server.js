971c650e7b94a6d9afea251f1d2bcc1650884a4a
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-09-04 17:57:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,24 @@ const neo4j = require('neo4j-driver')
 
 const port = 5000
 const uri = 'bolt://localhost'
+const user = 'neo4j'
+const password = 'neo4j'
 //Middleware//
 app.use(bodyParser.json());
 //Middleware//
 
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
-
+const session = driver.session()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  
+  session.run(
+    'MATCH (n) RETURN n LIMIT 25'
+    ).then(() => {
+      
+    }).catch();
+    
+    res.send('Neo4j connection status: active')
 })
 
 app.listen(port, () => {
