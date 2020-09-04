af8f56a8df4792a79f0fa97e067510420a0c3f66
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-09-04 18:50:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,20 +17,40 @@ const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
 const session = driver.session()
 
 app.get('/', (req, res) => {
-  
+
   session.run(
     'MATCH (n) RETURN n LIMIT 25'
-    ).then((result) => {
-      result.records.forEach((record) => {
-        console.log(record._fields[0].properties)
-      })
-    }).catch((err) => {
-      console.log("error", err);
-      res.send("Error")
-    });
-    
-    res.send('Neo4j connection status: active')
+  ).then((result) => {
+    result.records.forEach((record) => {
+      console.log(record._fields[0].properties)
+    })
+  }).catch((err) => {
+    console.log("error", err);
+    res.send("Error")
+  });
+
+  res.send('Neo4j connection status: active')
 })
+
+app.get('/avengers', (req, res) => {
+
+  var avengers = []
+  session.run(
+    'MATCH (a:Person)-[r:MemberOf]->(b:Group {name: "Avengers"}) RETURN a'
+  ).then((result) => {
+    result.records.forEach((record) => {
+      console.log(record._fields[0].properties)
+      avengers.push(record._fields[0].properties.name)
+    })
+    console.log("Avengers: ", avengers)
+    res.send('Avengers: ' + avengers)
+
+  }).catch((err) => {
+    console.log("error", err);
+    res.send("Error")
+  });
+})
+
 
 app.listen(port, () => {
   console.log(`Neo4j server listening at http://localhost:${port}`)
